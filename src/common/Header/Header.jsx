ee193ac482f77d39000c6b7da96cc85082814882
Caribla <caribla4@gmail.com>
2024-04-06 12:57:08 +0200

    Feat: Login => Login interface and function implemented and success generating redux slice

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 export const Header = () => {
     const location = useLocation()
-    if(location.pathname !== "/" && location.pathname !== "/register"){
+    if(location.pathname !== "/accounts/login" && location.pathname !== "/accounts/register"){
         return (
             <div className="headerDesign">
                 Im header
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,108 @@
-import "./Login.css"
+import "./Login.css";
+import { decodeToken } from "react-jwt";
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../app/modules/userModules";
+import { loginService } from "../../services/apiCalls";
+import { InputAuth } from "../../common/InputAuth/InputAuth";
+import { ButtonAuth } from "../../common/ButtonAuth/ButtonAuth";
+import { GoogleButton } from "../../common/GoogleButton/GoogleButton";
 
 export const Login = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [success, setSuccess] = useState(false)
+    const [msgSuccess, setMsgSuccess] = useState("")
+    const [notAllowToLogin, setNotAllowToLogin] = useState(true)
+
+    const [credentials, setCredentials] = useState({
+        email : "",
+        password : ""
+    })
+
+    useEffect(() => {
+        if(credentials.email !== "" && credentials.password !== ""){
+            setNotAllowToLogin(false)
+        } else {
+            setNotAllowToLogin(true)
+        }
+    }, [credentials])
+
+    const registerRedirect = () => {
+        navigate("/accounts/register")
+    }
+
+    const loginCredentialsHandler = (e) => {
+        setCredentials((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const loginUserCall = async () => {
+        try {
+            const fetched = await loginService(credentials)
+            if(fetched.success === false){
+                throw new Error("Invalid credentials")
+            }
+            setSuccess(true)
+            const decodedToken = decodeToken(fetched.token)
+
+            const passport = {
+                token: fetched.token,
+                decoded: decodedToken,
+            }
+
+            setMsgSuccess("")
+            dispatch(login({ credentials: passport }))
+        } catch (error) {
+            setSuccess(false)
+            setMsgSuccess(error.message)
+        }
+    }
+
     return (
         <div className="loginDesign">
-            Im the login
+            <div className="gifLogin">
+                <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2E1Ynl0anlqOGtod2tkMzlveGZocHdxbDY5aW0zcmdncGk0dW1iMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7aDd4hAEcciOzcVW/giphy.gif"/>
+            </div>
+            <div>
+            <div className="loginForm">
+                <h1 className="appName">Fakebook</h1>
+                <InputAuth 
+                    className={"inputAuthForm"} 
+                    type={"email"}
+                    name={"email"}
+                    value={credentials.email || ""} 
+                    placeholder={"User or email"}
+                    onChange={e => loginCredentialsHandler(e)}
+                />
+                <InputAuth
+                    className={"inputAuthForm"} 
+                    type={"password"}
+                    name={"password"}
+                    value={credentials.password || ""} 
+                    placeholder={"Password"}
+                    onChange={e => loginCredentialsHandler(e)}
+                />
+                <ButtonAuth
+                    className={"buttonAuthDesign"}
+                    buttonText={"Login"}
+                    onClickFunction={loginUserCall}
+                    disabled={notAllowToLogin}
+                />
+                <div className={(!success ? "loginError" : "loginSuccess")}>{msgSuccess}</div>
+                <div className="line"/>
+                <GoogleButton/>
+                <div className="forgetPassword">
+                    <a className="registerLink">Do you forget your password?</a>
+                </div>
+            </div>
+            <div className="registerContent">
+                <p className="registerText">You do not have an account?</p> <a className="registerLink" onClick={registerRedirect}>Register</a>
+            </div>
+            </div>
         </div>
     )
 }
