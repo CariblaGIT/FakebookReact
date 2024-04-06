10396017714478c7cbde4ab053128e05ca1ba37d
Caribla <caribla4@gmail.com>
2024-04-06 19:57:46 +0200

    Feat: Timeline => Redirecting if user is not logged correctly

--- src/pages/Timeline/Timeline.jsx
+++ src/pages/Timeline/Timeline.jsx
@@ -1,6 +1,19 @@
-import "./Timeline.css"
+import "./Timeline.css";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { userData } from "../../app/modules/userModules";
 
 export const Timeline = () => {
+    const navigate = useNavigate()
+    const userLogged = useSelector(userData);
+
+    useEffect(() => {
+        if (!userLogged.credentials.token) {
+            navigate("/accounts/login");
+        }
+    }, [userLogged]);
+    
     return (
         <div className="timelineDesign">
             Im the timeline
