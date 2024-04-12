7e706da8ccc2c62b28bec483535e1e6a10e10cd7
Caribla <caribla4@gmail.com>
2024-04-12 09:09:30 +0200

    Feat: Profile => Update profile checking credentials and able to back to profile

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,9 +1,43 @@
-import "./UpdateProfile.css"
+import "./UpdateProfile.css";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { userData } from "../../app/modules/userModules";
 
 export const UpdateProfile = () => {
+    const navigate = useNavigate()
+    const userToken = (useSelector(userData)).credentials.token
+    const userDataToken = (useSelector(userData)).credentials.decoded
+    const imgsRoot = "https://fakebook-production-b29b.up.railway.app/api/public/"
+
+    useEffect(() => {
+        if (!userToken) {
+            navigate("/accounts/login")
+        }
+    }, [userToken])
+
+    const backToProfile = () => {
+        navigate("/profile")
+    }
+
     return (
         <div className="updateProfileDesign">
-            I'm the update profile view
+            <div className="headerUpdateProfile">
+                <i onClick={() => backToProfile()} className="bi bi-arrow-left arrowIconUpdateProfile"></i>
+                <h4 className="headerUpdateProfileText">Edit profile</h4>
+            </div>
+            <div className="avatarsSectionUpdateProfile">
+                <div className="profileIconUpdateProfile">
+                    <img className="userUpdateProfileIcon" src={`${imgsRoot}avatar/${userDataToken.avatar}`}/>
+                </div>
+                <div className="profileAvatarUpdateProfile"></div>
+            </div>
+            <div className="linkOpenAvatarFile">
+                <a>Select avatar file</a>
+            </div>
+            <div className="formUserDataToUpdate">
+                
+            </div>
         </div>
     )
 }
