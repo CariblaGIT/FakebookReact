b2a2c990325b8408e57a41e1fa19e92defe3e199
Caribla <caribla4@gmail.com>
2024-04-12 09:36:38 +0200

    Feat: Profile => Update profile implementing fields for user properties

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -3,12 +3,17 @@ import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userData } from "../../app/modules/userModules";
+import { InputProfile } from "../../common/InputProfile/InputProfile";
 
 export const UpdateProfile = () => {
     const navigate = useNavigate()
     const userToken = (useSelector(userData)).credentials.token
     const userDataToken = (useSelector(userData)).credentials.decoded
     const imgsRoot = "https://fakebook-production-b29b.up.railway.app/api/public/"
+    const [userProfileData, setUserProfileData] = useState({
+        user: "",
+        avatar: ""
+    })
 
     useEffect(() => {
         if (!userToken) {
@@ -36,7 +41,22 @@ export const UpdateProfile = () => {
                 <a>Select avatar file</a>
             </div>
             <div className="formUserDataToUpdate">
-                
+                <InputProfile
+                    inputName="Name" 
+                    type="text" 
+                    name="user"
+                    value={userProfileData.user || ""}
+                    placeholder="Write your username"
+                    onChange={() => console.log("Update")}
+                />
+                <InputProfile
+                    inputName="Description" 
+                    type="text" 
+                    name="description"
+                    value="lorem ipsum dolor sit amen"
+                    placeholder="Write a bio"
+                    disabled="disabled"
+                />
             </div>
         </div>
     )
