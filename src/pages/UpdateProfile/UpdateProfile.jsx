2c3f23d7e9f3e34a9701b779f7fa9870e190bc85
Caribla <caribla4@gmail.com>
2024-04-12 11:09:59 +0200

    Feat: Profile => Detecting changes on update profile and inserting button

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -4,13 +4,22 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userData } from "../../app/modules/userModules";
 import { InputProfile } from "../../common/InputProfile/InputProfile";
+import { getProfileService } from "../../services/apiCalls";
 
 export const UpdateProfile = () => {
     const navigate = useNavigate()
     const userToken = (useSelector(userData)).credentials.token
     const userDataToken = (useSelector(userData)).credentials.decoded
     const imgsRoot = "https://fakebook-production-b29b.up.railway.app/api/public/"
+    const [loadedUserProfile, setLoadedUserProfile] = useState(false)
+    const [changesDetected, setChangesDetected] = useState("disabled")
+    
     const [userProfileData, setUserProfileData] = useState({
+        user: "",
+        avatar: ""
+    })
+
+    const [userProfilePrevData, setUserProfilePrevData] = useState({
         user: "",
         avatar: ""
     })
@@ -21,8 +30,44 @@ export const UpdateProfile = () => {
         }
     }, [userToken])
 
+    useEffect(() => {
+        const getUserProfileData = async () => {
+            try {
+                const fetched = await getProfileService(userToken)
+                setLoadedUserProfile(true)
+                setUserProfileData({
+                    user: fetched.data.name,
+                    avatar: userDataToken.avatar
+                })
+                setUserProfilePrevData({
+                    user: fetched.data.name,
+                    avatar: userDataToken.avatar
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        if(!loadedUserProfile) { getUserProfileData() }
+    }, [userProfileData])
+
+    useEffect(() => {
+        if(JSON.stringify(userProfileData) !== JSON.stringify(userProfilePrevData)){
+            setChangesDetected("")
+        } else {
+            setChangesDetected("disabled")
+        }
+    }, [userProfileData, userProfilePrevData])
+
     const backToProfile = () => {
         navigate("/profile")
+    }
+
+    const detectChanges = (e) => {
+        setUserProfileData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
     }
 
     return (
@@ -47,7 +92,7 @@ export const UpdateProfile = () => {
                     name="user"
                     value={userProfileData.user || ""}
                     placeholder="Write your username"
-                    onChange={() => console.log("Update")}
+                    onChange={e => detectChanges(e)}
                 />
                 <InputProfile
                     inputName="Description" 
@@ -57,6 +102,9 @@ export const UpdateProfile = () => {
                     placeholder="Write a bio"
                     disabled="disabled"
                 />
+                <button className="saveLocalChanges" disabled={changesDetected}>
+                    SAVE <i className="bi bi-save-fill saveButtonIcon"></i>
+                </button>
             </div>
         </div>
     )
