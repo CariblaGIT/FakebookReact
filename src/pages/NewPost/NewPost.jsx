625c54a2be6bf5a209323633f53503dd880e7cdd
Caribla <caribla4@gmail.com>
2024-04-15 02:52:35 +0200

    Feat: Posts => Styling interface with the neccessary components to do a post

--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -7,6 +7,7 @@ import { userData } from "../../app/modules/userModules";
 export const NewPost = () => {
     const navigate = useNavigate()
     const userToken = (useSelector(userData)).credentials.token
+    const [imageSrc, setImageSrc] = useState(null)
 
     useEffect(() => {
         if (!userToken) {
@@ -20,9 +21,24 @@ export const NewPost = () => {
 
     return (
         <div className="newPostDesign">
-            <div className="headerUpdateProfile">
-                <i onClick={() => backToTimeline()} className="bi bi-arrow-left arrowIconUpdateProfile"></i>
-                <h4 className="headerUpdateProfileText">New Post</h4>
+            <div className="headerNewPost">
+                <i onClick={() => backToTimeline()} className="bi bi-arrow-left arrowIconNewPost"></i>
+                <h4 className="headerNewPostText">New Post</h4>
+            </div>
+            <div className="contentNewPost">
+                <div className="contentSelector">
+                    {imageSrc === null ? (
+                        <>Select an image</>
+                    ) : (
+                        <img className="contentImgPost" src={imageSrc} alt="Selected Image"/>
+                    )}
+                </div>
+            </div>
+            <div className="descriptionNewPost">
+                <textarea className="descriptionAreaNewPost" placeholder="Write your description" name="description" id="description" cols="30" rows="10"></textarea>
+            </div>
+            <div className="actionsNewPost">
+                <button className="buttonAddNewPost">Add Post</button>
             </div>
         </div>
     )
